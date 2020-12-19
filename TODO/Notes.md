2f71b884bd5b21dc1c8755db504a2a0576892acb
shreeda <shreeda@astaqc.com>
2020-12-19 17:10:23 +0530

    Initial commit

---
+++ TODO/Notes.md
@@ -0,0 +1,19 @@
+## Notes 
+
+This is a note for further development of this project 
+
+1. Creating a Python Package(need to learn how to publish a package using docker)
+
+2. Creating a code coverage for the present application.
+
+## What needed now 
+
+1. Postgresql
+2. Removing the github workflow 
+
+
+## Contribution 
+
+Anybody can contribute. Open to contribute
+
+Please make sure to PR 
---
+++ app/core/tests/test_models.py
@@ -0,0 +1,68 @@
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+from core import models 
+def sample_user(email = "test@namaskar.in", password = "passwordtest"):
+    """Create a sample user"""
+    return get_user_model().objects.create_user(email,password)
+
+
+
+class ModelTests(TestCase):
+
+    def test_create_user_with_email_successful(self):
+        """ Test creating a new user with an email is successful"""
+        email = "test@namaskar.in"
+        password = "Testpass123"
+        user = get_user_model().objects.create_user(
+            email = email,
+            password = password,
+        )
+
+        self.assertEqual(user.email,email)
+        self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalize(self):
+        """ Test the email for a new user is normalized"""
+        
+        email = "test@Namaskar.in"
+        user = get_user_model().objects.create_user(email,'test123')
+
+        self.assertEqual(user.email,email.lower())
+
+    def test_new_user_invalid_email(self):
+        """Test creating user with no email raise error"""
+
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, 'test123')
+
+        
+    def test_create_super_user(self):
+        """Test Creating a new superuser"""
+
+        user = get_user_model().objects.create_superuser(
+            'test@namaskar.in',
+            'test123'
+        )
+
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
+
+
+    def test_tag_str(self):
+        """Test the tag string  representation"""
+        tag = models.Tag.objects.create(
+            user = sample_user(),
+            name = "Egg"
+        )
+
+        self.assertEqual(str(tag),tag.name)
+
+    def test_ingredient_str(self):
+        """Test the ingredient string representation"""
+        ingredient = models.Ingredient.objects.create(
+            user = sample_user(),
+            name = 'super'
+        )
+
+        self.assertEqual(str(ingredient),ingredient.name)
